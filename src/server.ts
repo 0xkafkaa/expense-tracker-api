3f32dd4e80f70bc563ce8a71aaa46f79bff5aabc
Manoprasanth MATHAVAN <manoprasanth.mathavan@gmail.com>
2025-02-20 11:07:35 +0530

    add cors

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,11 @@ import express from "express";
 import "dotenv/config";
 import authRoutes from "./routes/authRoutes";
 import expenseRoutes from "./routes/expenseRoutes";
-
+import cors from "cors";
 import cookieParser from "cookie-parser";
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use(cookieParser());
 app.use("/auth", authRoutes);
 app.use("/expense", expenseRoutes);
